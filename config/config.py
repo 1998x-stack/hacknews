8e7677271999ea278e1e1bc5cd265c2630086c57
1998x-stack <2484230700@qq.com>
2024-09-29 15:29:05 +0800

    update

--- config/config.py
+++ config/config.py
@@ -7,10 +7,11 @@ TIMEOUT = 3
 PROXIES = {
     'https' : f"http://{os.environ['PROXY']}",
     'http' : f"http://{os.environ['PROXY']}",
-} if 'PROXIES' in os.environ else {}
+} if 'PROXY' in os.environ else {}
 SMTP_SERVER = os.environ.get('SMTP_SERVER', 'smtp.gmail.com')
 SMTP_PORT = int(os.environ.get('SMTP_PORT', 587))
 EMAIL_ADDRESS = os.environ['EMAIL_ADDRESS'] if 'EMAIL_ADDRESS' in os.environ else ''
 EMAIL_PASSWORD = os.environ['EMAIL_PASSWORD'] if 'EMAIL_PASSWORD' in os.environ else ''
 TO_EMAILS = os.environ['TO_EMAILS'] if 'TO_EMAILS' in os.environ else ''
+TO_EMAILS = TO_EMAILS.strip(',')
 TO_EMAILS = TO_EMAILS.split(',')
